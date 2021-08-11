9ad7be5446230bb6d787417f523d7dd9acc835e0
a.sivak <riziy_bes@mail.ru>
2021-08-11 07:46:33 +0300

    init

---
+++ app/js/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+window.addEventListener('load', function () {
+
+    (function loader() {
+        if (!document.querySelector('.loader')) {
+            return;
+        }
+
+        const loader = document.querySelector('.loader');
+
+        if (loader.classList.contains('active')) {
+            loader.classList.remove('active');
+        }
+
+        setTimeout(() => {
+            loader.parentElement.removeChild(loader);
+        }, 500);
+
+    })();
+
+});

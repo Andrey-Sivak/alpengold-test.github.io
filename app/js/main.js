78cac3c87075514bf4e1fbbe1bebab981421598a
a.sivak <riziy_bes@mail.ru>
2021-08-13 01:11:08 +0300

    next

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,21 @@ window.addEventListener('resize', () => {
     isMobile = checkWidth();
 });
 
+setTimeout(() => {
+    if (!document.querySelector('.loader')) {
+        return;
+    }
+
+    const loader = document.querySelector('.loader');
+    if (loader.classList.contains('active')) {
+        loader.classList.remove('active');
+
+        setTimeout(() => {
+            loader.parentElement.removeChild(loader);
+        }, 300)
+    }
+}, 2500);
+
 window.addEventListener('load', function () {
 
     (function loader() {
